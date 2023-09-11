40807b9816b4bd99b71923d187bc8f9b7521d62a
Dongyuni <nunsaram26@gmail.com>
2023-09-10 23:51:09 -0700

    created FoodCard.js and implemented on HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import FoodCard from '../components/FoodCard'
 
 const HomeScreen = () => {
 
     return (
         <SafeAreaView>
             <View>
-                <Text>Home Page</Text>
+                <FoodCard />
             </View>
         </SafeAreaView>
     )
